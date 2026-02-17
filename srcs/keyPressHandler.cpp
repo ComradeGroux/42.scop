758deaa4827ab7f2ee4814b5527607a122ed99f3
Victor Groux <victorgroux99@gmail.com>
2026-02-17 10:34:16 +0100

    Added some function to the math library
    Added the object movement on every axis in both direction
      - PAGE UP / PAGE DOWN
        move the object up and down
      - UP / DOWN
        move the object to the back / front
      - LEFT / RIGHT
        move the object to the left or to the right

--- srcs/keyPressHandler.cpp
+++ srcs/keyPressHandler.cpp
@@ -8,11 +8,12 @@
 #include "glMath.hpp"
 
 static	void	keyPress_PolygonMode(GLFWwindow* window, Shader* shader);
-
+static	void	keyPress_MoveObject(GLFWwindow* window, Object* obj);
 
 void	keyPressHandler(GLFWwindow* window, Camera* camera, Object* obj, Shader* shader)
 {
 	keyPress_PolygonMode(window, shader);
+	keyPress_MoveObject(window, obj);
 }
 
 void	keyPress_PolygonMode(GLFWwindow* window, Shader* shader)
@@ -23,4 +24,32 @@ void	keyPress_PolygonMode(GLFWwindow* window, Shader* shader)
 		shader->setRenderMode(Shader::WIREFRAME);
 	else if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
 		shader->setRenderMode(Shader::POINT);
+}
+
+void	keyPress_MoveObject(GLFWwindow* window, Object* obj)
+{
+	if (glfwGetKey(window, GLFW_KEY_PAGE_UP) == GLFW_PRESS)
+	{
+		mat4_move(obj->model, 0.0f, 0.05f, 0.0f);
+	}
+	else if (glfwGetKey(window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS)
+	{
+		mat4_move(obj->model, 0.0f, -0.05f, 0.0f);
+	}
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	{
+		mat4_move(obj->model, 0.0f, 0.0f, -0.05f);
+	}
+	else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+	{
+		mat4_move(obj->model, 0.0f, 0.0f, 0.05f);
+	}
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+	{
+		mat4_move(obj->model, -0.05f, 0.0f, 0.0f);
+	}
+	else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+	{
+		mat4_move(obj->model, 0.05f, 0.0f, 0.0f);
+	}
 }

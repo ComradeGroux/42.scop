322b8fc83ac1feeaeb73d11fa3b11cb4d30dacbd
Victor Groux <victorgroux99@gmail.com>
2026-02-17 09:14:00 +0100

    Light modification in the BMP loader

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -48,7 +48,7 @@ GLuint loadBMP(const char* filepath)
 		return 0;
 	}
 
-	unsigned char header[54];
+	unsigned char	header[54];
 	if (fread(header, 1, 54, file) != 54) {
 		fclose(file);
 		return 0;
@@ -59,24 +59,34 @@ GLuint loadBMP(const char* filepath)
 		return 0;
 	}
 
-	unsigned int dataPos = *(int*)&(header[0x0A]);
-	unsigned int width = *(int*)&(header[0x12]);
-	unsigned int height = *(int*)&(header[0x16]);
-	unsigned int imageSize = *(int*)&(header[0x22]);
+	unsigned int	dataPos, width, height, imageSize;
+	unsigned short	bitsPerPixel;
+	memcpy(&dataPos,		&header[0x0A], 4);
+	memcpy(&width,			&header[0x12], 4);
+	memcpy(&height,			&header[0x16], 4);
+	memcpy(&imageSize,		&header[0x22], 4);
+	memcpy(&bitsPerPixel,	&header[0x1C], 2);
 
 	std::cout << "BMP chargé: " << width << "x" << height << std::endl;
 	std::cout << "Taille image: " << imageSize << std::endl;
 
+	if (bitsPerPixel != 24)
+	{
+		std::cerr << "BMP 32 bits detected" << std::endl;
+		fclose(file);
+		return 0;
+	}
 	if (imageSize == 0) imageSize = width * height * 3;
 	if (dataPos == 0) dataPos = 54;
 
 	unsigned char* data = new unsigned char[imageSize];
 
-	size_t tmp = fread(data, 1, imageSize, file);
-	(void)tmp;
+	fseek(file, dataPos, SEEK_SET);
+	size_t t = fread(data, 1, imageSize, file);
+	(void)t;
 	fclose(file);
 
-	for (unsigned int i = 0; i < imageSize; i += 3) {
+	for (unsigned int i = 0; i + 2 < imageSize; i += 3) {
 		unsigned char tmp = data[i];
 		data[i] = data[i + 2];
 		data[i + 2] = tmp;
@@ -93,7 +103,7 @@ GLuint loadBMP(const char* filepath)
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	delete[] data;
